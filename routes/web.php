e06fea3b66632009c62b1d46114fcc285098f0d6
Ogar <ogarjobbassey@gmail.com>
2023-08-15 12:16:38 +0100

    Include `users` in web routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class)->name('home');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard',            DashboardController::class)->name('dashboard');
+    Route::get('/dashboard',      DashboardController::class)->name('dashboard');
+    Route::resource('users',    UserController::class)->only('index', 'show');
+
 });
 
