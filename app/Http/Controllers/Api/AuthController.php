c83bc7167bff4ec1560d960fbd7e1dffb7f4cb2a
Ogar <ogarjobbassey@gmail.com>
2023-08-15 08:57:49 +0100

    Create `AuthController`

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function store(Request $request)
+    {
+        if (! Auth::attempt(request()->only(['email', 'password']), request()->boolean('remember'))) {
+            throw ValidationException::withMessages(['email' => 'Invalid login credentials!']);
+        }
+
+        $request->session()->regenerate();
+
+        user()->update(['login_count' => user()->login_count + 1, 'last_login' => now()]); // Todo: Move to an event listener
+
+        return Response::api('Welcome back...');
+    }
+}
