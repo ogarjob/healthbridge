581be97ec1dda76d8c2ed851b2bb1eb4556fad5e
Ogar <ogarjobbassey@gmail.com>
2023-08-15 09:01:30 +0100

    Create `Department` model with factory and migration

---
+++ database/migrations/2023_08_14_212226_create_departments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            /*$table->string('image')->nullable();
+            $table->integer('relevance')->default(0);
+            $table->timestamp('featured_at')->nullable();*/
+            $table->authors();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('departments');
+    }
+};
