81d896997a9c14e9a3149176bfdde9a45bd326b0
Ogar <ogarjobbassey@gmail.com>
2023-08-15 09:04:06 +0100

    Create `Role` model with factory and migrations

---
+++ database/migrations/2014_10_12_000001_add_authors_to_roles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            $table->authors();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropColumns('roles', ['created_by', 'updated_by']);
+    }
+};
