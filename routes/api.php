7dcae3b94775a95c8712026e030a2792fe8edbb3
Ogar <ogarjobbassey@gmail.com>
2023-08-16 20:21:32 +0100

    Add Auth api routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\AppointmentController;
+use App\Http\Controllers\Api\AppointmentStatusController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\DepartmentController;
 use App\Http\Controllers\Api\NewUserController;
+use App\Http\Controllers\Api\TransactionController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +24,12 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login',     [AuthController::class, 'store'])->name('login');
 Route::post('/register',  [NewUserController::class, 'store'])->name('register');
 Route::delete('/logout',  [AuthController::class, 'destroy'])->name('logout');
+
+Route::apiResource('appointments',  AppointmentController::class)->only('store');
+
+Route::middleware('auth')->group(function () {
+    Route::apiResource('users',                         UserController::class)->only('store', 'update', 'destroy');
+    Route::apiresource('departments',                   DepartmentController::class)->only('store', 'update', 'destroy');
+    Route::apiResource('appointments.status',           AppointmentStatusController::class)->only('store');
+    Route::apiResource('appointments.transactions',     TransactionController::class)->only('store');
+});
