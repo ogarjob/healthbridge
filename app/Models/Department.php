ca363ca3373583bc9e52d31ea65b10c1a7d27512
Ogar <ogarjobbassey@gmail.com>
2023-08-16 20:10:00 +0100

    Create booted method in `Department` model

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Department extends Model
 {
     use HasFactory;
+
+    public static function booted()
+    {
+        static::creating(function ($department) {
+            $department->slug = Str::slug($department->name);
+        });
+    }
 }
