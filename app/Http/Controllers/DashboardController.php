37392338e698d647af7e9c609d0221a6e7e58f57
Ogar <ogarjobbassey@gmail.com>
2023-08-16 19:59:26 +0100

    Update `DashboardController`

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Order;
-use App\Models\Product;
+use App\Models\Appointment;
+use App\Models\Department;
 use App\Models\User;
 
 class DashboardController extends Controller
 {
     public function __invoke()
     {
-        return view('dashboard.index');
+        $appointments         = user()->isAdmin() ? Appointment::take(50)->get() : user()->appointments;
+        $patients_count       = User::patient()->count();
+        $administrators_count = User::admin()->count();
+        $departments_count    = Department::count();
+        $appointments_count   = Appointment::count();
+
+        return view('dashboard.index',
+            compact(['appointments', 'patients_count', 'departments_count', 'appointments_count', 'administrators_count'])
+        );
     }
 }
