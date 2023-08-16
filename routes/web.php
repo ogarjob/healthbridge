cd51b1a0071ad2e68dcbace3dd937433dabff4e2
Ogar <ogarjobbassey@gmail.com>
2023-08-16 20:21:58 +0100

    Add Auth web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserAppointmentController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,8 +22,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class)->name('home');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard',      DashboardController::class)->name('dashboard');
-    Route::resource('users',    UserController::class)->only('index', 'show');
+    Route::get('/dashboard',                    DashboardController::class)->name('dashboard');
+    Route::resource('users',                UserController::class)->only('index', 'show');
+    Route::resource('departments',          DepartmentController::class)->only('index');
+    Route::resource('appointments',         AppointmentController::class)->only('index', 'show');
+    Route::resource('users.appointments',   UserAppointmentController::class)->only('index');
+    Route::singleton('settings',            SettingController::class)->only('edit');
 
 });
 
