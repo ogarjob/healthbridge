9f6a0466b22e36a2eac0551c46f3b81ef71515b5
Ogar <ogarjobbassey@gmail.com>
2023-08-17 13:46:11 +0100

    Update `DatabaseSeeder`

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,21 +17,49 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        Department::factory()->create(['name' => 'Department 1']);
+        Department::factory()->create(['name' => 'ANAESTHESIOLOGY']);
+        Department::factory()->create(['name' => 'AUDIOLOGIST']);
+        Department::factory()->create(['name' => 'CARDIOLOGY']);
+        Department::factory()->create(['name' => 'DENTISTRY']);
+        Department::factory()->create(['name' => 'DERMATOLOGY']);
+        Department::factory()->create(['name' => 'DIETETICS']);
+        Department::factory()->create(['name' => 'ENDOCRINOLOGY']);
+        Department::factory()->create(['name' => 'GASTROENTEROLOGY']);
+        Department::factory()->create(['name' => 'HAEMATOLOGY']);
+        Department::factory()->create(['name' => 'NEPHROLOGY']);
+        Department::factory()->create(['name' => 'NEUROLOGY']);
+        Department::factory()->create(['name' => 'OPHTHALMOLOGY']);
+        Department::factory()->create(['name' => 'OPTOMETRY']);
+        Department::factory()->create(['name' => 'ORTHODONTICS']);
+        Department::factory()->create(['name' => 'ORTHOPAEDICS']);
+        Department::factory()->create(['name' => 'PHYSIOTHERAPY']);
+        Department::factory()->create(['name' => 'PSYCHIATRY']);
+        Department::factory()->create(['name' => 'PULMONOLOGY']);
+        Department::factory()->create(['name' => 'RHEUMATOLOGY']);
+        Department::factory()->create(['name' => 'UROLOGY']);
 
         User::factory()->create([
             'id'         => 1,
-            'first_name' => 'Og',
-            'last_name'  => 'Bassey',
+            'first_name' => 'Ogar',
+            'last_name'  => 'Job',
             'email'      => 'ask4rapzo@gmail.com'
         ])->appointments()->create(['id' => 1, 'department_id' => 1])->update(['created_by' => 1]);
 
         User::factory()->create([
             'id'         => 2,
-            'first_name' => 'Ogar',
-            'last_name'  => 'Job',
+            'first_name' => 'Joseph',
+            'last_name'  => 'Bassey',
             'email'      => 'ogarjobbassey@gmail.com',
             'phone'      => '08126012460',
+        ])->update(['created_by' => 1]);
+
+        User::factory()->create([
+            'id'         => 3,
+            'first_name' => 'Samuel',
+            'last_name'  => 'Mashok',
+            'type'       => 'patient',
+            'email'      => 'sammmash@gmail.com',
+            'phone'      => '08126012678',
         ])->update(['created_by' => 1]);
 
         Status::factory()->create(['name' => 'Confirmed', 'sequence' => 1])->update(['created_by' => 1]);
