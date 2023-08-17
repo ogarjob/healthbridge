74e3dca568aa01495af4142b24542cba14e43481
Ogar <ogarjobbassey@gmail.com>
2023-08-17 14:17:19 +0100

    Include `set-locale` in api routes

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Api\DepartmentController;
 use App\Http\Controllers\Api\NewUserController;
 use App\Http\Controllers\Api\TransactionController;
 use App\Http\Controllers\Api\UserController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\SetLocaleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,8 +24,9 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login',     [AuthController::class, 'store'])->name('login');
 Route::post('/register',  [NewUserController::class, 'store'])->name('register');
 Route::delete('/logout',  [AuthController::class, 'destroy'])->name('logout');
+Route::post('/set-locale', SetLocaleController::class)->name('set-locale');
 
-Route::apiResource('appointments',  AppointmentController::class)->only('store');
+Route::apiResource('appointments',          AppointmentController::class)->only('store');
 
 Route::middleware('auth')->group(function () {
     Route::apiResource('users',                         UserController::class)->only('store', 'update', 'destroy');
